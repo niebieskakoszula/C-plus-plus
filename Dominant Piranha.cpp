a330b3c33bd5ab2f84713b7e47e98f8204fcb396
niebieskakoszula <62652473+niebieskakoszula@users.noreply.github.com>
2021-06-25 17:19:06 +0200

    Update Dominant Piranha.cpp

--- Dominant Piranha.cpp
+++ Dominant Piranha.cpp
@@ -1,41 +1,55 @@
 //https://codeforces.com/contest/1433/problem/C
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-vector <int> A;
+vector <int> pirahnas_sizes;
+int amount_of_piranhas;
+int biggest_size;
+
+void Read_Piranhas_Sizes() {
+    for (int i = 0; i < amount_of_piranhas; i++)
+        cin >> pirahnas_sizes[i];
+}
+
+void Find_Biggest_Piranha() {
+    biggest_size = 0;
+    for (int i = 0; i < amount_of_piranhas; i++)
+        if (pirahnas_sizes[i] > biggest_size)
+            biggest_size = pirahnas_sizes[i];
+}
+
+int Find_Dominant_Pirahna_Position() {
+    int position = -1;
+    for (int i = 0; i < amount_of_piranhas; i++) {
+        if (pirahnas_sizes[i] != biggest_size) {
+            continue;
+        }
+        if (i - 1 >= 0 && pirahnas_sizes[i - 1] < biggest_size) {
+            position = i + 1;
+            break;
+        }
+        if (i + 1 < amount_of_piranhas && pirahnas_sizes[i + 1] < biggest_size) {
+            position = i + 1;
+            break;
+        }
+    }
+    return position;
+}
 
 int main()
 {
-    int t;
-    cin >> t;
-    while ( t-- ) {
-        int n;
-        cin >> n;
-        A.resize(n);
-        int maxx=0;
-        for (int i = 0; i < n; i++) {
-            cin >> A[i];
-            if(A[i] > maxx) {
-                maxx = A[i];
-            }
-        }
-        int pos = -1;
-        for (int i = 0; i < n; i++) {
-            if(A[i] != maxx) {
-                continue;
-            }
-            if(i-1 >= 0 && A[i-1] < maxx) {
-                pos = i + 1;
-                break;
-            }
-            if(i+1 < n && A[i+1] < maxx) {
-                pos = i + 1;
-                break;
-            }
-        }
-        cout << pos << endl;
+    int tests;
+    cin >> tests;
+    while (tests--) {
+        cin >> amount_of_piranhas;
+        pirahnas_sizes.resize(amount_of_piranhas);
+        Read_Piranhas_Sizes();
+        Find_Biggest_Piranha();
+
+        cout << Find_Dominant_Pirahna_Position() << endl;
     }
     return 0;
 }
