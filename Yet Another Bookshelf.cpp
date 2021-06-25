8b5614dcb39fb25c15d0b2a6436747eec413af63
niebieskakoszula <62652473+niebieskakoszula@users.noreply.github.com>
2021-06-25 22:06:33 +0200

    Update Yet Another Bookshelf.cpp

--- Yet Another Bookshelf.cpp
+++ Yet Another Bookshelf.cpp
@@ -4,40 +4,46 @@
 
 using namespace std;
 
+int shelf_size;
+int books[50];
+
+
+void Read_Input(){
+    for (int i = 0; i < shelf_size; i++)
+        cin >> books[i];
+}
+
+int Find_First_Book() {
+    for (int i = 0; i < shelf_size; i++)
+        if (books[i] == 1)
+            return i;
+}
+int Find_Last_Book() {
+    for (int i = shelf_size - 1; i >= 0; i--)
+        if (books[i] == 1)
+            return i;
+}
+int Count_Minimum_Moves(int start, int end_index){
+    int min_moves = 0;
+    for (int i = start; i < end_index; i++) {
+        if (books[i] == 0) min_moves++;
+    }
+    return min_moves;
+}
+
 int main()
 {
-    int t, n, x, min_moves, first, last;
-    cin>>t;
-    while(t--){
-        cin>>n;
-        bool books[n];
-        x=0;
-        for(int i=0; i<n; i++){
-            cin>>books[i];
-            if(books[i]==1)x++;
-        }
-        if(x==1 || x==n) cout<<0<<endl;
-        else{
-            min_moves=0;
-            for(int i=0; i<n; i++){
-                if(books[i]==1){
-                    first=i;
-                    break;
-                }
-            }
-            for(int i=n-1; i>=0; i--){
-                if(books[i]==1){
-                    last=i;
-                    break;
-                }
-            }
-            for(int i=first+1; i<last; i++){
-                if(books[i]==0){
-                    min_moves++;
-                }
-            }
-            cout<<min_moves<<endl;
-        }
+    int tests, first_book_index, last_book_index;
+    cin >> tests;
+    for(int j = 0; j < tests; j++) {
+        cin >> shelf_size;
+        Read_Input();
+        first_book_index = Find_First_Book();
+        last_book_index = Find_Last_Book();
+
+        cout << Count_Minimum_Moves(first_book_index, last_book_index) << endl;
     }
+
     return 0;
 }
+
