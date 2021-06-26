f2c58d65401a0391f9d774731d4bd6a08a09750a
niebieskakoszula <62652473+niebieskakoszula@users.noreply.github.com>
2021-06-26 22:30:48 +0200

    Update Dyzio.cpp

--- Dyzio.cpp
+++ Dyzio.cpp
@@ -1,24 +1,24 @@
 //https://pl.spoj.com/problems/DYZIO2/
 
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-const int  n=1000000;
-int values[n+1];
-bool S[n+1];
- 
-int main()
-{
-    unsigned int N, p, q, x;
+
+const int  range = 1000000;
+int values[range + 1];
+bool S[range + 1];
+
+void Generate_Primes(){
+    int p, q, x;
     values[1]=0;
-    for(int i = 2; i <= n; i++ ) S [ i ] = true;
+
+    for(int i = 2; i <= range; i++ ) S [ i ] = true;
     p = 2;
-    while( p * p <= n ){
+    while( p * p <= range ){
         q = p;
-        while( p * q <= n ){
+        while( p * q <= range ){
             x = p * q;
-            while( x <= n ){
+            while( x <= range ){
                 S [ x ] = false;
                 x *= p;
             }
@@ -26,16 +26,22 @@ int main()
         }
     while( !S [ ++p ] );
     }
-    for(int i = 2; i <= n+1; i++ ){
-        if( S [ i ] ) values[i]=values[i-1]+1;
-        else values[i]=values[i-1];
+    for(int i = 2; i <= range + 1; i++ ){
+        if(S[i]) values[i] = values[i - 1] + 1;
+        else values[i] = values[i - 1];
     }
- 
-    cin>>N;
-    while(N--){
-        int start,endd;
-        cin>>start>>endd;
-        cout<<values[endd]-values[start-1]<<endl;
+}
+
+int main()
+{
+    Generate_Primes();
+    int tests;
+    cin >> tests;
+    while(tests--){
+        int start, endd;
+        cin >> start >> endd;
+        cout << values[endd] - values[start-1] << endl;
     }
     return 0;
 }
+
